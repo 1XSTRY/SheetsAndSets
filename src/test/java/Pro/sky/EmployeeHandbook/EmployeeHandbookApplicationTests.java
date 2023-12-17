320f1ff795bdf46332bea5ef3e422674b8feeeb5
1XSTRY <kosogornov99@gmail.com>
2023-12-17 16:13:20 +0500

    initial commit

---
+++ src/test/java/Pro/sky/EmployeeHandbook/EmployeeHandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package Pro.sky.EmployeeHandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeHandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
